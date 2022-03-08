615a50b19f0cd41a9bc18c264e83742c0bf26591
Quareco <alex_clarinete123@hotmail.com>
2022-03-07 22:33:55 -0300

    atualização

---
+++ funcao.js
@@ -0,0 +1,46 @@
+
+function SomaNumeros (a , b){
+
+    console.log ("A soma do resultado é: ", a + b);
+    
+}
+
+function SubNum (a , b){
+
+    console.log("A subtração do resultado é: ", a - b);
+
+}
+
+function MulNum (a , b){
+
+    console.log("O resultado da multiplicação é: ", a * b);
+
+}
+
+function DivNum (a , b){
+
+    console.log("O resultado da divisão é: ", a / b, "\nE sobrou: ", a%b);
+
+}
+/////////////////////////////////////////////////////////////////////// FUNCION TXT ///////////////////////////////////////////////////////////////////////////////////////////////
+
+function testeNome (nome){
+
+    console.log("O seu nome é: ", nome);
+
+}
+
+/////////////////////////////////////////////////////////////////////// AREA DE TESTES /////////////////////////////////////////////////////////////////////////////////////////////////
+
+SomaNumeros (3, 18);
+SubNum (3, 18);
+MulNum (3, 18);
+DivNum (4, 2);
+
+testeNome("Carlos");
+
+
+// const teste = (a , b) => a - b * 3;
+
+// console.log(teste(2 , 3));
+
